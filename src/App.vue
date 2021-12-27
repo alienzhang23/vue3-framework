fb591c184c7a709f71194e9c9439542574785b09
zhangxingwei <zhangxingwei@hailiang.com>
2021-12-27 14:24:30 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,82 @@
 
 <style lang="less">
 
+body,
+html,
+li,
+input,
+gloabl,
+h2,
+th,
+input,
+nav,
+td,
+h1,
+embed,
+blockquote,
+ol,
+xml,
+div,
+dl,
+dt,
+dd,
+ul,
+h3,
+h4,
+h5,
+h6,
+pre,
+form,
+fieldset,
+abbr,
+p,
+header,
+hgroup,
+Section,
+figure,
+section,
+nav,
+article,
+aside,
+canvas {
+  margin: 0;
+  padding: 0;
+  font-family: MicrosoftYaHeiUI;
+  box-sizing: border-box;
+}
+
+body {
+  width: 100%;
+  height: 100%;
+}
+
+ol,
+ul,
+li {
+  list-style: none;
+}
+
+input,
+textarea {
+  -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+  -webkit-appearance: none;
+}
+.el-table th.gutter {
+  display: table-cell !important;
+}
+.el-message-box {
+  .el-message-box__container {
+    text-align: center;
+    .el-message-box__status {
+      display: none;
+    }
+    .el-message-box__message {
+      padding: 0;
+    }
+  }
+  .el-message-box__btns {
+    text-align: center;
+    margin-top: 20px;
+  }
+}
 </style>
